5eb08a7108cac102f26e33b7be8e075fe3b0eb86
Sachin Verma <CosmoAI@users.noreply.huggingface.co>
2023-08-27 15:01:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,32 @@
 from hugchat import hugchat
-import streamlit as st
+from hugchat.login import Login
+import streamlit as st 
 
-cookie_path = 'cookies.json'
-chatbot = hugchat.ChatBot(cookie_path=cookie_path)
+# Log in to huggingface and grant authorization to huggingchat
+sign = Login(mecosmo369@gmail.com, @_3SegZXGjrjff/)
+cookies = sign.login()
 
-response = chatbot.start_conversation('Hello!')
-print(response)
+# Save cookies to the local directory
+cookie_path_dir = "./cookies_snapshot"
+sign.saveCookiesToDir(cookie_path_dir)
 
-# chatbot = hugchat.ChatBot(cookie_path="home/user/cookies.json")
-# with st.chat_message:
-#     st.markdown(chatbot.chat("Hi"))
+# Create a ChatBot
+chatbot = hugchat.ChatBot(cookies=cookies.get_dict())  # or cookie_path="usercookies/<email>.json"
+print(chatbot.chat("Hi!"))
+
+# Create a new conversation
+id = chatbot.new_conversation()
+chatbot.change_conversation(id)
+
+# Get conversation list
+conversation_list = chatbot.get_conversation_list()
+if uinput := st.chat_input("How can I help you?"):
+    with st.chat_message:
+        st.markdown(chatbot.chat(uinput))
     
-# # Create a new conversation
-# id = chatbot.new_conversation()
-# chatbot.change_conversation(id)
 
-# # Get conversation list
-# conversation_list = chatbot.get_conversation_list()
-# uinput = st.chat_input("How can I help you?")
-# with st.chat_message:
-#     st.markdown(chatbot.chat(uinput))
-    
+# # Switch model (default: meta-llama/Llama-2-70b-chat-hf. )
+# chatbot.switch_llm(0) # Switch to `OpenAssistant/oasst-sft-6-llama-30b-xor`
+# chatbot.switch_llm(1) # Switch to `meta-llama/Llama-2-70b-chat-hf`
+
+
