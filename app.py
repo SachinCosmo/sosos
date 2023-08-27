de66a206af780145a31aa76cc0840799b48f5276
Sachin Verma <CosmoAI@users.noreply.huggingface.co>
2023-08-27 10:33:33 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from hugchat import hugchat
+import streamlit as st
+
 chatbot = hugchat.ChatBot(cookie_path="cookies.json")
-print(chatbot.chat("Hi"))
+with st.chat_message:
+    st.markdown(chatbot.chat("Hi"))
+    
 # Create a new conversation
 id = chatbot.new_conversation()
 chatbot.change_conversation(id)
+
 # Get conversation list
 conversation_list = chatbot.get_conversation_list()
-print(chatbot.chat("what is the most important thing to know as a human"))
+uinput = st.chat_input("How can I help you?")
+with st.chat_message:
+    st.markdown(chatbot.chat(uinput))
+    
