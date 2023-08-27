49678ab662d8dd58d652fe3825cb498cf9844f36
SachinCosmo <mecosmo369@gmail.com>
2023-08-28 03:16:45 +0530

    this

--- app.py
+++ app.py
@@ -1,40 +1,5 @@
 import streamlit as st
 
 # Add a custom CSS style for the background video and centered button
-st.markdown(
-    """
-    <html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<style>
-  body {
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-  }
-  video {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    min-width: 100%;
-    min-height: 100%;
-    width: auto;
-    height: auto;
-    z-index: -1;
-  }
-</style>
-<title>Fullscreen Video Example</title>
-</head>
-<body>
-<video autoplay loop muted playsinline>
-  <source src="media/universe.mp4" type="video/mp4">
-  Your browser does not support the video tag.
-</video>
-</body>
-</html>
-    """,
-    unsafe_allow_html=True
-)
+st.button("Explore")
 
