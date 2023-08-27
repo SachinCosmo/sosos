039df46a44740a5a0e6d693fb49f69c6b9d4cb61
Sachin Verma <CosmoAI@users.noreply.huggingface.co>
2023-08-27 21:39:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,43 +3,37 @@ import streamlit as st
 # Add a custom CSS style for the background video and centered button
 st.markdown(
     """
-    <style>
-    .fullscreen-bg {
-        position: fixed;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        overflow: hidden;
-        z-index: -1;
-    }
-    .fullscreen-bg video,
-    .fullscreen-bg img {
-        width: 100%;
-        height: auto;
-    }
-    .centered-button {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        z-index: 1;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-# Add the background video or GIF
-st.markdown(
-    """
-    <div class="fullscreen-bg">
-        <video loop muted autoplay playsinline>
-            <source src="universe.mp4" type="video/mp4">
-            Your browser does not support the video tag.
-        </video>
-        <button class="centered-button">Explore Infinite</button>
-    </div>
+    <html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+  body {
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
+  video {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    min-width: 100%;
+    min-height: 100%;
+    width: auto;
+    height: auto;
+    z-index: -1;
+  }
+</style>
+<title>Fullscreen Video Example</title>
+</head>
+<body>
+<video autoplay loop muted playsinline>
+  <source src="universe.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
+</body>
+</html>
     """,
     unsafe_allow_html=True
 )
