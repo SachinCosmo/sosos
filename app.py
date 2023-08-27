722bd72be65257441a6c17e5948eb062d9e7a45f
Sachin Verma <CosmoAI@users.noreply.huggingface.co>
2023-08-27 10:38:11 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ chatbot = hugchat.ChatBot(cookie_path="cookies.json")
 with st.chat_message:
     st.markdown(chatbot.chat("Hi"))
     
-# Create a new conversation
-id = chatbot.new_conversation()
-chatbot.change_conversation(id)
+# # Create a new conversation
+# id = chatbot.new_conversation()
+# chatbot.change_conversation(id)
 
-# Get conversation list
-conversation_list = chatbot.get_conversation_list()
-uinput = st.chat_input("How can I help you?")
-with st.chat_message:
-    st.markdown(chatbot.chat(uinput))
+# # Get conversation list
+# conversation_list = chatbot.get_conversation_list()
+# uinput = st.chat_input("How can I help you?")
+# with st.chat_message:
+#     st.markdown(chatbot.chat(uinput))
     
