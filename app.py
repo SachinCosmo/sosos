3ddd352d4a90bafbd7ccc873dcf03d399d0a86b7
Sachin Verma <CosmoAI@users.noreply.huggingface.co>
2023-08-27 12:58:25 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 from hugchat import hugchat
 import streamlit as st
 
-chatbot = hugchat.ChatBot(cookie_path="home/user/cookies.json")
-with st.chat_message:
-    st.markdown(chatbot.chat("Hi"))
-    
-# Create a new conversation
-id = chatbot.new_conversation()
-chatbot.change_conversation(id)
+cookie_path = 'cookies.json'
+chatbot = hugchat.ChatBot(cookie_path=cookie_path)
 
-# Get conversation list
-conversation_list = chatbot.get_conversation_list()
-uinput = st.chat_input("How can I help you?")
-with st.chat_message:
-    st.markdown(chatbot.chat(uinput))
+response = await chatbot.start_conversation('Hello!')
+print(response)
+
+# chatbot = hugchat.ChatBot(cookie_path="home/user/cookies.json")
+# with st.chat_message:
+#     st.markdown(chatbot.chat("Hi"))
+    
+# # Create a new conversation
+# id = chatbot.new_conversation()
+# chatbot.change_conversation(id)
+
+# # Get conversation list
+# conversation_list = chatbot.get_conversation_list()
+# uinput = st.chat_input("How can I help you?")
+# with st.chat_message:
+#     st.markdown(chatbot.chat(uinput))
     
