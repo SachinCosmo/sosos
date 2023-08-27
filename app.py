63ab97ba75fbd098ecb7bc2fbc9eb393983ee3e6
Sachin Verma <CosmoAI@users.noreply.huggingface.co>
2023-08-27 20:23:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,55 +1,67 @@
 import streamlit as st
-from hugchat import hugchat
-from hugchat.login import Login
+from streamlit_option_menu import option_menu
+import json
+from Home import dashboard
 
-# App title
-st.set_page_config(page_title="🤗💬 HugChat")
 
-# Hugging Face Credentials
-with st.sidebar:
-    st.title('🤗💬 HugChat')
-    if ('EMAIL' in st.secrets) and ('PASS' in st.secrets):
-        st.success('HuggingFace Login credentials already provided!', icon='✅')
-        hf_email = st.secrets['EMAIL']
-        hf_pass = st.secrets['PASS']
-    else:
-        hf_email = st.text_input('Enter E-mail:', type='password')
-        hf_pass = st.text_input('Enter password:', type='password')
-        if not (hf_email and hf_pass):
-            st.warning('Please enter your credentials!', icon='⚠️')
+
+st.page_config(page_title="Auth", page_icon=":lock:")
+
+
+def loadfile():
+    with open("database/users.json") as file:
+        data = json.load(file)
+    return data
+
+def savefile(data):
+    with open("database/users.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+
+
+def login():
+    st.write("Login")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    if st.button("Login"):
+        data = loadfile()
+        if username in data:
+            if data[username]["password"] == password:
+                st.success("Logged In as {}".format(username))
+                st.session_state.user = username
+            else:
+                st.error("Wrong Password")
         else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
-    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-an-llm-powered-chatbot-with-streamlit/)!')
-    
-# Store LLM generated responses
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
+            st.error("User not found")
+            
+            
+def register():
+    st.write("Register")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    if st.button("Register"):
+        data = loadfile()
+        if username in data:
+            st.error("User already exists")
+        else:
+            data[username] = {}
+            data[username]["password"] = password
+            savefile(data)
+            st.success("User created")
 
-# Display chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
 
-# Function for generating LLM response
-def generate_response(prompt_input, email, passwd):
-    # Hugging Face Login
-    sign = Login(email, passwd)
-    cookies = sign.login()
-    # Create ChatBot                        
-    chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
-    return chatbot.chat(prompt_input)
 
-# User-provided prompt
-if prompt := st.chat_input(disabled=not (hf_email and hf_pass)):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.write(prompt)
 
-# Generate a new response if last message is not from assistant
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            response = generate_response(prompt, hf_email, hf_pass) 
-            st.write(response) 
-    message = {"role": "assistant", "content": response}
-    st.session_state.messages.append(message)
+def main():
+    if 'user' not in st.session_state:
+        st.session_state.user = None
+        
+    if st.session_state.user is None:
+        with st.sidebar:
+            selected = option_menu(None, ['Login', 'Register'])
+            if selected == 'Login':
+                login()
+            elif selected == 'Register':
+                register()
+    else:
+        dashboard()
