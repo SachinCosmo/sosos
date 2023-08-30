c87e94de1fe6cb5c753d452b5d68847d69377058
SachinCosmo <101745239+SachinCosmo@users.noreply.github.com>
2023-08-30 07:26:44 +0530

    Update Auth.py

--- Auth.py
+++ Auth.py
@@ -5,7 +5,7 @@ from Home import dashboard
 # import pymongo
 
 
-st.page_config(page_title="Auth", page_icon=":lock:") 
+st.set_page_config(page_title="Auth", page_icon=":lock:") 
 
 
 # from pymongo.mongo_client import MongoClient
