62bf8845ee5a057e7e5bb56c592c964082957446
SachinCosmo <mecosmo369@gmail.com>
2023-08-30 11:34:45 +0530

    emti

--- emti.py
+++ emti.py
@@ -1,18 +1,18 @@
-import pymongo
+# import pymongo
 
 
-uri = "mongodb+srv://<cosmo>:<cosmo>@cluster0.nfkbemi.mongodb.net/?retryWrites=true&w=majority"
+# uri = "mongodb+srv://<cosmo>:<cosmo>@cluster0.nfkbemi.mongodb.net/?retryWrites=true&w=majority"
 
-client = pymongo.MongoClient(uri)
+# client = pymongo.MongoClient(uri)
 
 
-db = client['Cosmo']
+# db = client['Cosmo']
 
-col = db['Users']
+# col = db['Users']
 
-test = {
-    "name": "Cosmo",
-    "age": 21,
-}
+# test = {
+#     "name": "Cosmo",
+#     "age": 21,
+# }
 
-col.insert_one(test)
+# col.insert_one(test)
