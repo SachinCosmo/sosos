8b3efefd1e57c645f82f3989a5ab5b30298c0463
SachinCosmo <101745239+SachinCosmo@users.noreply.github.com>
2023-08-31 12:04:26 +0530

    Update Auth.py

--- Auth.py
+++ Auth.py
@@ -22,7 +22,7 @@ st.markdown(hide, unsafe_allow_html=True)
 
 
 
-uri = secrets.MONGO_CONNECTION_STRING
+uri = os.environ("MONGO_CONNECTION_STRING")
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
